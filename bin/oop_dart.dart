7a3e4b7be6874aac19c301961b6b40f12edfac73
amirhaikaal <haikalamir221@gmail.com>
2023-04-14 15:55:13 +0700

    Pertemuan 3

---
+++ bin/oop_dart.dart
@@ -0,0 +1,24 @@
+import 'package:oop_dart/oop_dart.dart' as oop_dart;
+import 'package:oop_dart/class.dart';
+
+void main() {
+  var programmer = new ManusiaMilenial('Iqbal');
+  programmer.email = 'iqbal@bsi.ac.id';
+  programmer.info();
+  programmer.makan();
+
+  print('\n');
+
+  var dokter = new ManusiaMilenial('Haikal');
+  dokter.email = 'haikal@bsi.ac.id';
+  dokter.info();
+  dokter.makan();
+
+  print('\n');
+
+  var hacker = new ManusiaMilenial('Dimas');
+  hacker.email = 'dimas@bsi.ac.id';
+  hacker.info();
+  hacker.makan();
+}
+Footer
